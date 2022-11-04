a20305780462ed29866770fdbb2fbd3aae19b982
BigSev15 <palat008@umn.edu>
2022-11-04 13:20:56 -0500

    wrote manager tests and class

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,10 +1,33 @@
 const manager = require('../lib/manager');
 
-xdescribe("manager", () => {
+describe("manager", () => {
     describe("Initialization", () => {
         it("Should return a new 'manager' object", () => {
             const testE = new manager();
             expect(testE instanceof manager).toEqual(true);
         });
-    })
-})
+
+        it("Should set name, email, id, and officeNo when created", () => {
+            const name = 'Seve';
+            const email = 'seve@email.com';
+            const id = 15;
+            const officeNo = 5;
+
+            const testE = new manager('Seve', 'seve@email.com', 15, 5);
+
+            expect(testE.name).toEqual(name);
+            expect(testE.email).toEqual(email);
+            expect(testE.id).toEqual(id);
+            expect(testE.officeNo).toEqual(officeNo);
+        });
+    });
+
+    describe("getRole", () => {
+        it("Should return role when getRole() is called on manager object", () => {
+            const testE = new manager('Seve', 'seve@email.com', 15, 5);
+            const role = 'Manager';
+            eRole = testE.getRole();
+            expect(eRole).toEqual(role);
+        });
+    });
+});
