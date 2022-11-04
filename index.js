0ad31fe59c9782149f02a4e21def1e4cddfaaf7a
BigSev15 <palat008@umn.edu>
2022-11-04 14:34:42 -0500

    finished the prompts and logic to handle different team member roles

--- index.js
+++ index.js
@@ -29,6 +29,76 @@ function addMember() {
             name: 'role',
             message: 'Please enter team member role',
             choices: ['Manager', 'Engineer', 'Intern']
+        },
+    ])
+    .then((data) => {
+        if (data.role === 'Manager') {
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'office',
+                    message: "Please enter manager's office number"
+                }
+            ])
+            .then((data2) => {
+                const teamManager = new manager(data.name, data.email, data.id, data2.office);
+                team.push(teamManager);
+                Continue();
+            });
+        } else if (data.role === 'Engineer') {
+            inquirer
+            .prompt([
+                {
+                    type:'input',
+                    name: 'github',
+                    message: "Please enter engineer's github username"
+                }
+            ])
+            .then((data3) => {
+                const teamEngineer = new engineer(data.name, data.email, data.id, data3.github);
+                team.push(teamEngineer);
+                Continue();
+            });
+        } else {
+            inquirer
+            .prompt([
+                {
+                    type:'input',
+                    name:'school',
+                    message: "Please enter intern's school"
+                }
+            ])
+            .then((data4) => {
+                const teamIntern = new intern(data.name, data.email, data.id, data4.school);
+                team.push(teamIntern);
+                Continue();
+            });
+        };
+    });
+};
+
+function Continue() {
+    inquirer
+    .prompt([
+        {
+            type: 'confirm',
+            name: 'continue',
+            message: 'Add more team members?'
         }
     ])
+    .then((data) => {
+        if (data.continue) {
+            addMember();
+        } else {
+            console.log(team);
+        };
+    });
+};
+
+function init() {
+    addMember();
 }
+
+init();
+
